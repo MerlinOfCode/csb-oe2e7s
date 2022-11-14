c7eb0a74e58c738dbb7e7758c143dfa189a4c0e9
Code Sandbox <hello@codesandbox.io>
2022-11-14 17:57:47 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.93bbcedca4e7a1ac955cfa1a4fc19b31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb30901c7e68ccbed892",
+    "url": "https://thewizardofwikacy.github.io/csb-oe2e7s/static/css/main.7ae45fe4.chunk.css"
+  },
+  {
+    "revision": "bb30901c7e68ccbed892",
+    "url": "https://thewizardofwikacy.github.io/csb-oe2e7s/static/js/main.bb30901c.chunk.js"
+  },
+  {
+    "revision": "f2f22e9a8b0b5dcb4faa",
+    "url": "https://thewizardofwikacy.github.io/csb-oe2e7s/static/js/1.f2f22e9a.chunk.js"
+  },
+  {
+    "revision": "31baa261294061042596",
+    "url": "https://thewizardofwikacy.github.io/csb-oe2e7s/static/js/runtime~main.31baa261.js"
+  },
+  {
+    "revision": "4698f3b60090244ae844cc0c41ea154d",
+    "url": "https://thewizardofwikacy.github.io/csb-oe2e7s/index.html"
+  }
+];
